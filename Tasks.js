a273aab2e54a6e9675eb7147e3eacbad4f77c8fb
mohamedlafiteh <m_lafiteh@yahoo.com>
2020-02-20 20:13:14 +0000

    Add new changes

--- Tasks.js
+++ Tasks.js
@@ -1,7 +1,11 @@
 function oddNumber(num) {
+  let changeNumToString = num.toString().length;
+  console.log(changeNumToString);
+  let numbersLengthTwo = [];
   for (var i = 0; i < num; i++) {
-    if (i % 2 !== 0) {
-      console.log("this is ood " + i);
+    if (changeNumToString === 2) {
+      numbersLengthTwo.push(i);
+      console.log("this is numbers mo " + numbersLengthTwo);
     }
   }
 }
